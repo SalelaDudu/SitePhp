8a6479675e1d7ab5cd1a6621a1944ff4c9e96374
Eduardo Santos <coronelcheetos@gmail.com>
2023-05-16 13:44:05 -0300

    customização em progresso, ainda bem simples e com erros propositais para testar algumas posições

--- assets/php/header.php
+++ assets/php/header.php
@@ -5,20 +5,20 @@
 
   <div id="header_div_left">
     <a href="" class="header_a" id="projetos_a"> Projetos </a>
-    <a href="" class="header_icon" id="icon_a"> <img src="" alt=""> </a>
+    <a href="https://github.com/SalelaDudu/" target="_blank" class="header_icon" id="icon_a"> <img src="assets/resources/github.png" alt="logo do github arredondado"> </a>
   </div>
 
   <!-- elemento centralizado -->
 
   <div id="header_div_center">
-    <h3 class="tittle_3" id="header_tittle3"> Nosso Site</h3>
+    <h3 class="tittle_3" id="header_tittle3"> Seu Site</h3>
   </div>
 
   <!-- elementos à direita -->
 
   <div id="header_div_right">
-    <a class="alto_contraste" id="alto_contraste_1">box</a>
-    <a class="alto_contraste" id="alto_contraste_2">box</a>
-    <a class="alto_contraste" id="alto_contraste_3">box</a>
+    <div class="alto_contraste"><a id="alto_contraste_1">box</a></div>
+    <div class="alto_contraste"><a id="alto_contraste_1">box</a></div>
+    <div class="alto_contraste"><a id="alto_contraste_1">box</a></div>
   </div>
 </header>
